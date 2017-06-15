40607b96250ef16faf588335c63b73959de2f8b1
ratul <ratulcse27@gmail.com>
2017-06-15 18:34:03 +0600

    remove resource

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>Metronic Admin Theme #1 | User Login 1</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Preview page of Metronic Admin Theme #1 for " name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    <link href="../assets/pages/css/login.min.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <!-- END THEME LAYOUT STYLES -->
+    <link rel="shortcut icon" href="favicon.ico" /> </head>
+<!-- END HEAD -->
+
+<body class=" login">
+<!-- BEGIN LOGO -->
+<div class="logo">
+    <a href="index.html">
+        <img src="../assets/pages/img/logo-big.png" alt="" /> </a>
+</div>
+<!-- END LOGO -->
+<!-- BEGIN LOGIN -->
+<div class="content">
+    <!-- BEGIN LOGIN FORM -->
+    <form class="login-form" action="index.html" method="post">
+        <h3 class="form-title font-green">Sign In</h3>
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Username</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username" /> </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Password</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password" /> </div>
+        <div class="form-actions">
+            <button type="submit" class="btn green uppercase">Login</button>
+            <a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
+        </div>
+        <div class="create-account">
+            <p>
+                <a href="javascript:;" id="register-btn" class="uppercase">Create an account</a>
+            </p>
+        </div>
+    </form>
+    <!-- END LOGIN FORM -->
+
+</div>
+<div class="copyright"> 2014 © Metronic. Admin Dashboard Template. </div>
+<!--[if lt IE 9]>
+<script src="../assets/global/plugins/respond.min.js"></script>
+<script src="../assets/global/plugins/excanvas.min.js"></script>
+<script src="../assets/global/plugins/ie8.fix.min.js"></script>
+<![endif]-->
+<!-- BEGIN CORE PLUGINS -->
+<script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/',['as'=>'test','uses'=>'Auth\AuthController@test']);
 
 Route::group(['namespace' => 'Auth'],function (){
     Route::get('login',['as'=>'web.login','uses'=>'AuthController@login']);
-    Route::get('login',['as'=>'web.do.login','uses'=>'AuthController@doLogin']);
+    Route::post('login',['as'=>'web.do.login','uses'=>'AuthController@doLogin']);
     Route::get('register',['as'=>'web.register','uses'=>'AuthController@register']);
-    Route::get('register',['as'=>'web.do.register','uses'=>'AuthController@doRegister']);
+    Route::post('register',['as'=>'web.do.register','uses'=>'AuthController@doRegister']);
 });
